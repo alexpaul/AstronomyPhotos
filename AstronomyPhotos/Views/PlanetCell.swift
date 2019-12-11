28821f71ec8d5a468b1d339c5c3f36998f287cd8
alexpaul <alexpaulnyc@gmail.com>
2019-12-11 05:22:42 -0500

    cell now keeps track of it's url image string and prevents dequeue cell flickering

--- AstronomyPhotos/Views/PlanetCell.swift
+++ AstronomyPhotos/Views/PlanetCell.swift
@@ -11,17 +11,40 @@ import UIKit
 class PlanetCell: UITableViewCell {
 
   @IBOutlet weak var planetImageView: UIImageView!
+  // objective: stop flickering while dequeue resusable cells
+  // solution: keep track of the image url string by using a string variable on the cell
+  
+  private var urlString = ""
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    // empty out the image view
+    planetImageView.image = nil
+  }
   
   func configureCell(with urlString: String) {
-    planetImageView.getImage(with: urlString) { result in
+    
+    // set the cell's urlString
+    self.urlString = urlString
+    
+    planetImageView.getImage(with: urlString) { [weak self] result in
       switch result {
       case .failure:
         DispatchQueue.main.async {
-          self.planetImageView.image = UIImage(systemName: "person.fill")
+          self?.planetImageView.image = UIImage(systemName: "exclamationmark.triangle")
         }
       case .success(let image):
         DispatchQueue.main.async {
-          self.planetImageView.image = image
+          
+          // only if the cell's urlString is the same as the one being
+          // passed in from the cellForRowAt will we change the current
+          // image view's image
+          // 1. self.urlSring is the cell's property
+          // 2. urlString is the argument from our configureCell method
+          if self?.urlString == urlString {
+            self?.planetImageView.image = image
+          }
+          
         }
       }
     }
